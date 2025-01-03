fff1ab63300857efb9698e1b2d26b180db0ce127
Dragon-Knight <dubki4132@mail.ru>
2025-01-03 04:05:47 +0300

    fix ADC calibration logic

--- src/EasyPinA.cpp
+++ src/EasyPinA.cpp
@@ -4,15 +4,15 @@ void EasyPinA::Init()
 {
 	HAL_GPIO_WritePin(_port, _pin.Pin, GPIO_PIN_RESET);
 	HAL_GPIO_Init(_port, &_pin);
-	Calibration();
-
+	
+	HAL_ADC_Start(_hadc);
+	
 	return;
 }
 
 uint16_t EasyPinA::Get()
 {
 	HAL_ADC_ConfigChannel(_hadc, &_config);
-	//Calibration();
 	HAL_ADC_Start(_hadc);
 	HAL_ADC_PollForConversion(_hadc, 5);
 	return HAL_ADC_GetValue(_hadc);
@@ -20,11 +20,13 @@ uint16_t EasyPinA::Get()
 
 void EasyPinA::Calibration()
 {
-	#if defined(STM32F1)
-		HAL_ADCEx_Calibration_Start(_hadc);
-	#elif defined(STM32H7)
-		HAL_ADCEx_Calibration_Start(_hadc, ADC_CALIB_OFFSET, ADC_SINGLE_ENDED);
-	#endif
+	HAL_ADC_Stop(_hadc);
+#if defined(STM32F1)
+	HAL_ADCEx_Calibration_Start(_hadc);
+#elif defined(STM32H7)
+	HAL_ADCEx_Calibration_Start(_hadc, ADC_CALIB_OFFSET, ADC_SINGLE_ENDED);
+#endif
+	HAL_ADC_Start(_hadc);
 	
 	return;
 }
