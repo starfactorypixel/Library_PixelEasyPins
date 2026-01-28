4e17ddd941a2b7b0eb3a5b1f7f1d6e591f702e9a
Dragon-Knight <dubki4132@mail.ru>
2026-01-28 05:06:46 +0300

    EasyPinD: add support ESP32

--- src/EasyPinA.h
+++ src/EasyPinA.h
@@ -6,6 +6,40 @@
 #elif defined(STM32H7)
 	#include "stm32h7xx_hal.h"
 	#include "stm32h7xx_hal_adc.h"
+#elif defined(ESP32)
+
+	#define HAL_GPIO_WritePin(_port, _pin, GPIO_PIN_RESET)
+	#define HAL_GPIO_Init(_port, _pin)
+	#define HAL_ADC_Start(_hadc)
+	#define HAL_ADC_ConfigChannel(_hadc, _config)
+	#define HAL_ADC_PollForConversion(_hadc, time)
+	#define HAL_ADC_GetValue(_hadc) 0
+	#define HAL_ADC_Stop(_hadc)
+
+	#define GPIO_TypeDef uint32_t
+	#define ADC_HandleTypeDef uint32_t
+	#define ADC_SAMPLETIME_1CYCLE_5 15UL
+	
+	typedef struct
+	{
+		uint32_t Pin;
+		uint32_t Mode;
+		uint32_t Pull; 
+		uint32_t Speed;
+	} GPIO_InitTypeDef;
+	
+	typedef struct 
+	{
+		uint32_t Channel;
+		uint32_t Rank;
+		uint32_t SamplingTime;
+		}ADC_ChannelConfTypeDef;
+
+	#define GPIO_MODE_ANALOG 0
+	#define GPIO_NOPULL 0
+	#define GPIO_SPEED_FREQ_LOW 0
+	#define ADC_REGULAR_RANK_1 0
+
 #else
 	#error "Unsupported STM32 series."
 #endif
--- src/EasyPinD.h
+++ src/EasyPinD.h
@@ -4,6 +4,52 @@
 	#include "stm32f1xx_hal.h"
 #elif defined(STM32H7)
 	#include "stm32h7xx_hal.h"
+#elif defined(ESP32)
+	#include "driver/gpio.h"
+
+	#define GPIO_MODE_INPUT			GPIO_MODE_INPUT
+	#define GPIO_MODE_OUTPUT_PP		GPIO_MODE_OUTPUT
+	#define GPIO_MODE_OUTPUT_OD		GPIO_MODE_OUTPUT_OD
+	#define GPIO_MODE_AF_PP			GPIO_MODE_OUTPUT
+	#define GPIO_MODE_AF_OD			GPIO_MODE_OUTPUT_OD
+	#define GPIO_MODE_AF_INPUT		GPIO_MODE_INPUT
+
+	#define GPIO_NOPULL				0
+	#define GPIO_PULLUP				1
+	#define GPIO_PULLDOWN			2
+
+	#define GPIO_SPEED_FREQ_HIGH	0
+	#define GPIO_SPEED_FREQ_MEDIUM	1
+	#define GPIO_SPEED_FREQ_LOW		2
+
+	#define GPIO_PIN_RESET			0
+	#define GPIO_PIN_SET			1
+
+	#define GPIO_TypeDef			uint8_t
+	#define GPIO_PinState			uint8_t
+	typedef struct
+	{
+		gpio_num_t Pin;
+		gpio_mode_t Mode;
+		uint8_t Pull;
+		uint8_t Speed;
+	} GPIO_InitTypeDef;
+	
+	#define HAL_GPIO_Init(_port, _pin) \
+		gpio_config_t conf = { (1ULL << (_pin)->Pin), (_pin)->Mode, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE }; \
+		if((_pin)->Pull == GPIO_PULLUP) conf.pull_up_en = GPIO_PULLUP_ENABLE; \
+		if((_pin)->Pull == GPIO_PULLDOWN) conf.pull_down_en = GPIO_PULLDOWN_ENABLE; \
+		gpio_config(&conf);
+	
+	#define HAL_GPIO_WritePin(_port, _pin, _state) \
+		gpio_set_level((_pin), (_state))
+	
+	#define HAL_GPIO_ReadPin(_port, _pin) \
+		gpio_get_level((_pin))
+	
+	#define HAL_GPIO_TogglePin(_port, _pin) \
+		gpio_set_level((_pin), 1 - gpio_get_level((_pin)))
+	
 #else
 	#error "Unsupported STM32 series."
 #endif
@@ -18,7 +64,8 @@ class EasyPinD
 			uint16_t Pin;
 		} d_pin_t;
 		
-		// EasyPinD pin(GPIOA, {GPIO_PIN_0, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, GPIO_SPEED_FREQ_LOW}, GPIO_PIN_SET);
+		// STM32: EasyPinD pin(GPIOA, {GPIO_PIN_0, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, GPIO_SPEED_FREQ_LOW}, GPIO_PIN_SET);
+		// ESP32: EasyPinD pin(0, {GPIO_NUM_20, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH}, GPIO_PIN_SET);
 		EasyPinD(GPIO_TypeDef *port, GPIO_InitTypeDef pin, GPIO_PinState init = GPIO_PIN_RESET) : _port(port), _pin(pin), _state(init)
 		{}
 		
